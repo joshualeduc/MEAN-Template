64c80a45eda3f8cdfddc46063f46996e51ab3e8e
Joshua LeDuc <joshualeduc@outlook.com>
2016-04-13 19:14:24 -0600

    instructions for dbinfo

--- server/config/env/development.js
+++ server/config/env/development.js
@@ -1,4 +1,4 @@
-var mongodb = require('./dbInfo.js');
+var mongodb = require('./dbInfo.js'); //module.exports mongolab url in this file
 module.exports = {
   db: mongodb,
   sessionSecret: 'developmentSecret'
